8e9a97c31ee91f512b976bb094dc6ba26f624562
Jakub Krajewski <jakub.j.krajewski@gmail.com>
2023-02-13 08:15:05 +0000

    Blueprint for new selenium tests

---
+++ selenium_test.py
@@ -0,0 +1,23 @@
+def test_number_of_tournament_in_single_season():
+    """Test if number of competitions in the season are the same in DB and fis-web"""
+    pass
+
+
+def test_compare_number_of_jumpers():
+    """Test: compare number of jumpers in the file and fis-web, have to be the same number"""
+    pass
+
+
+def test_compare_names():
+    """Test if names are the same in the fis-web and the file."""
+    pass
+
+
+def test_compare_nationality():
+    """Test if nationality are the same in the fis-web and the file."""
+    pass
+
+
+def test_compare_points():
+    """Test if points in fis-web are the same as points in file"""
+    pass
